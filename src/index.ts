0ad0624c88acf5c333c32c929e07e573e03fab7c
Meyirzhan Nurlybekuly <meyirzhannurlybekuly@MacBook-Air-Meyirzhan.local>
2022-09-30 14:41:51 +0600

    rate limiter added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import rateLimiterMiddleware from '@middlewares/rate-limiter';
 import usersRouter from '@routes/users';
 
 const app = express();
@@ -7,9 +8,10 @@ const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
+app.use(rateLimiterMiddleware);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 
 // Routes
 app.use('/users/', usersRouter);
---
+++ src/middlewares/rate-limiter/index.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import { RateLimiterMemory } from 'rate-limiter-flexible';
+
+const rateLimiter = new RateLimiterMemory({
+    points: 6,
+    duration: 1,
+});
+
+const rateLimiterMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    rateLimiter
+        .consume(req.ip, 2)
+        .then(() => {
+            next();
+        })
+        .catch(() => {
+            res.status(429).send('Too Many Requests');
+        });
+};
+
+export default rateLimiterMiddleware;
